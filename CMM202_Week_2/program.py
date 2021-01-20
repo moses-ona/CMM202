2a2cdde765eaabc56b37df08824fadb0d533afa0
C M <c.moreno-garcia@rgu.ac.uk>
2021-01-20 15:00:17 +0000

    gg

---
+++ CMM202_Week_2/program.py
@@ -0,0 +1,34 @@
+#%% First, lets create a menu with two options
+# 1. enter your name
+# 2. count the number of "a's" in your name and show it
+
+
+#%% Then, we add the option "0" to exit the program
+
+#%% Then, we add some complexity
+# the name must have more than 2 characters
+# if the user enters an invalid name, gets kicked out
+
+#%% Finally, we give more robustness to our program
+# a) option 2 cannot be selected without option 1 being selected first
+# b) give three opportunities for the name before "kick out"
+# c) Verify that the input in the main menu is a number
+# if a number, print "invalid option"
+# if not a number, print "invalid input"
+
+
+
+
+
+
+
+
+
+
+
+
+#%% More ideas
+# Add a third option that displays all the names that have been stored before
+# and their corresponding "a" letter count
+# if this option is selected before 1 and 2, print "empty"
+# this option has to check that both 1 and 2 have been used before in that order
